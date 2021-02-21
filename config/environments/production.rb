7115ca5c4b992d2091833e92c6a257777f5fcd81
Julian Casaburi <juliancasaburi@gmail.com>
2021-02-21 20:16:15 -0300

    Se modifican los nombres de las dbs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -53,7 +53,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "ttps_ruby_tpi_unlp_2020_production"
+  # config.active_job.queue_name_prefix = "rubynotes_production"
 
   config.action_mailer.perform_caching = false
 
